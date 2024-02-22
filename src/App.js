ba918674f0c88595f2f30fea1ef098a3e47a6e72
Lakshay Gupta <lakshaygupta2511@gmail.com>
2024-02-23 00:31:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Footer, Possibility, Features, WhatGPT3, Header } from './containers';
+import { Brand, Navbar } from './components';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
+    </div>
+    <Brand />
+    <Features />
+    <Possibility />
+    <WhatGPT3 />
+    {/* <CTA />
+    <Blog /> */}
+    <Footer />
+  </div>
+);
+
+export default App;
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { google, slack, atlassian } from './imports';
+import './brand.css';
+
+const Brand = () => (
+  <div className="gpt3__brand section__padding">
+    <div className="logos">
+      <div className="logos-slide">
+        <img src={google} />
+        <img src={slack} />
+        <img src={atlassian} />
+        <img src={slack} />
+        <img src={atlassian} />
+        <img src={slack} />
+        <img src={atlassian} />
+        <img src={slack} />
+        <img src={atlassian} />
+        <img src={slack} />
+        <img src={atlassian} />
+        <img src={slack} />
+        <img src={atlassian} />
+        <img src={slack} />
+        <img src={atlassian} />
+        <img src={slack} />
+        <img src={atlassian} />
+        <img src={slack} />
+        <img src={atlassian} />
+        <img src={slack} />
+        <img src={atlassian} />
+        <img src={slack} />
+        <img src={atlassian} />
+        <img src={slack} />
+        <img src={atlassian} />
+      </div>
+    </div>
+    {/* <div>
+      <img src={google} />
+    </div>
+    <div>
+      <img src={slack} />
+    </div>
+    <div>
+      <img src={atlassian} />
+    </div>
+    <div>
+      <img src={dropbox} />
+    </div>
+    <div>
+      <img src={shopify} />
+    </div> */}
+  </div>
+);
+
+export default Brand;
